dbe7fae930d7585e55481bded90fd03028b2336d
LeticiaISilveira <leticia.ingrid.silveira@gmail.com>
2019-08-23 12:53:15 -0300

    Adição de função simples para cifrar e decifrar

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function message() {
+  event.preventDefault();
+    
+  /* Pegar os dados do HTML */
+  const str = document.getElementById("txt").value;
+  const offset = parseInt(document.getElementById("offset").value);
+
+  /* <select> <option> */
+  const option = document.getElementById("unittype");
+  const item = option.options[option.selectedIndex].value;
+    
+  /* Mostrar resultado de acordo com a opção selecionada */
+  if (item == "1") {
+    const rEncode = window.cipher.encode(offset, str);
+    document.getElementById("result").value = rEncode;
+
+  } else {
+    const rDecode = window.cipher.decode(offset,str);
+    document.getElementById("result").value = rDecode;
+  }
+}
+
+message();  
