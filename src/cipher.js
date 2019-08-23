365489d2f7ea208066668ad08517e4953cb14728
LeticiaISilveira <leticia.ingrid.silveira@gmail.com>
2019-08-23 12:53:15 -0300

    alterações

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,20 @@
 window.cipher = {
-  // ... 
+  encode: encodeMsg,
+  decode: decodeMsg,
 };
+
+function encodeMsg(offset, str) {
+  let resultE ="";
+  for (let i=0; i<str.length; i++) {
+    resultE += String.fromCharCode((str[i].charCodeAt(0)+offset-65)%26+65);
+  }
+  return resultE;
+}
+
+function decodeMsg(offset, str) {
+  let resultD ="";
+  for (let i=0; i<str.length; i++) {
+    resultD += String.fromCharCode((str[i].charCodeAt(0)-offset+65)%26+65);
+  }
+  return resultD;
+}
