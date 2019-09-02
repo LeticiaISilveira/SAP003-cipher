433161634034f4a425335b70c4323f149730e7f4
LeticiaISilveira <leticia.ingrid.silveira@gmail.com>
2019-09-02 11:14:43 -0300

    Limpar código

--- src/index.js
+++ src/index.js
@@ -2,36 +2,28 @@ document.getElementById("btn-enviar").addEventListener("click", message);
 
 function message(event) {
   event.preventDefault();
-    
-  /* Pegar os dados do HTML */
+
   const str = document.getElementById("txt").value;
   const offset = parseInt(document.getElementById("offset").value || 0);
 
-  /* <select> <option> */
   const option = document.getElementById("option");
   const item = option.options[option.selectedIndex].value;
-    
-  /* Mostrar resultado de acordo com a opção selecionada */
   if (item == "1") {
     const rEncode = window.cipher.encode(offset, str);
     document.getElementById("txt").value = rEncode;
-
   } else {
     const rDecode = window.cipher.decode(offset, str);
     document.getElementById("txt").value = rDecode;
   }
 }
-
-/*limpar e copiar*/
-let textArea = document.querySelector("#txt");
-const clear = document.querySelector("#clear");
-const copy = document.querySelector("#copy");
-
+ 
 clear.addEventListener("click", function () {
+  const textArea = document.getElementById("txt")
   textArea.value = "";
 }, false);
 
-copy.addEventListener("click", function() {
+copy.addEventListener("click", function () {
+  const textArea = document.getElementById("txt")
   textArea.select();
   textArea = document.execCommand("copy");
 });
